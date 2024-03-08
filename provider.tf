0eab65fb893ea73f3da9a9449933838e7e122e63
nani1415 <158695690+nani1415@users.noreply.github.com>
2024-03-08 21:10:33 +0530

    Update provider.tf
    acess key change

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-         access_key = "AKIAZPIA4YSLVU6ICKDG"
-		 secret_key = "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"
+         access_key = "AKIAU6GDYC6JD2RYRWPY"
+		 secret_key = "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"
 		 region = "ap-southeast-1"
 }
